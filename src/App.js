6b879432bb111bfa8ea034985fa50544d469e328
Ashley Kim <askim@hmc.edu>
2024-07-30 22:51:42 -0400

    Added a simple question and score block

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const questions = [
+    {
+      questionText: 'What is Ashley\'s last name?',
+      answerOptions: [
+        { answerText: 'Kim', isCorrect: true },
+        { answerText: 'Golden', isCorrect: false },
+        { answerText: 'Lee', isCorrect: false },
+        { answerText: 'K', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'What is Otis\'s last name?',
+      answerOptions: [
+        { answerText: 'Kim', isCorrect: false },
+        { answerText: 'Golden', isCorrect: true },
+        { answerText: 'Triangle', isCorrect: false },
+        { answerText: 'Ratio', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'What is this app made from?',
+      answerOptions: [
+        { answerText: 'Paper', isCorrect: false },
+        { answerText: 'Water', isCorrect: false },
+        { answerText: 'ChatGPT', isCorrect: false },
+        { answerText: 'ReactJS', isCorrect: true },
+      ],
+    },
+    {
+      questionText: 'What is the color of the sky?',
+      answerOptions: [
+        { answerText: 'Black', isCorrect: false },
+        { answerText: 'Red', isCorrect: false },
+        { answerText: 'Blue', isCorrect: true },
+        { answerText: 'Green', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'How old are you?',
+      answerOptions: [
+        { answerText: '20', isCorrect: true },
+        { answerText: '19', isCorrect: false },
+        { answerText: '21', isCorrect: false },
+        { answerText: '18', isCorrect: false },
+      ],
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const answerClickOption = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'app'>
+      {showScore ? (
+        <div className = 'score-section'>
+          You scored {score} out of {questions.length}
+        </div>
+        ) : (
+          <>
+            <div className = 'question-section'>
+              <div className = 'question-count'>
+                <span> Question {currentQuestion + 1} </span>/{questions.length}
+              </div>
+              <div className = 'question-text'>
+                {questions[currentQuestion].questionText}
+              </div>
+            </div>
+            <div className = 'answer-section'>
+              {questions[currentQuestion].answerOptions.map((answerOption) => (
+                <button onClick = {() => answerClickOption(answerOption.isCorrect)}>
+                  {answerOption.answerText}
+                </button>
+              ))}
+            </div>
+          </>
+      )}
     </div>
   );
 }
