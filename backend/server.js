e84872f57743dcd20e06c8fa14a3a1ef973ed7a5
oegolden <ogolden@g.hmc.edu>
2024-09-29 17:15:54 -0700

    changed port to 8080

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
